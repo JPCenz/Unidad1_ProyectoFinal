e0682729e918436472c50dfa49e44fcbff6ddb50
cesar <cquesr175@gmail.com>
2022-10-31 19:53:06 -0500

    se agregan 3 opciones

--- tarea1.py
+++ tarea1.py
@@ -22,15 +22,15 @@ def opcion2(lilibro):
         for i in leer:
             print(i[0])
 
-def opcion3(addlibro):
-    field_name = ['Libro','Id','Titulo','Genero','ISBN','Editorial','Autor']
-    dict = {"Libro":"libro4","Id": "444","Titulo": "titulo4","Genero": "genero4", "ISBN":"444-44-44444-44-4", "Editorial":"editorial4","Autor":"autor4"}
-    with open (addlibro, 'a') as agre_book:
-        dict_object = csv.DictWriter(agre_book, fieldnames=field_name)
-        dict_object.writerow(dict)
-        with open(agre_book, 'r') as read_agre_book:
-            lineas = read_agre_book.read().splitlines()
-            print(lineas)
+# def opcion3(addlibro):
+#     field_name = ['Libro','Id','Titulo','Genero','ISBN','Editorial','Autor']
+#     dict = {"Libro":"libro4","Id": "444","Titulo": "titulo4","Genero": "genero4", "ISBN":"444-44-44444-44-4", "Editorial":"editorial4","Autor":"autor4"}
+#     with open (addlibro, 'a') as agre_book:
+#         dict_object = csv.DictWriter(agre_book, fieldnames=field_name)
+#         dict_object.writerow(dict)
+#         with open(agre_book, 'r') as read_agre_book:
+#             lineas = read_agre_book.read().splitlines()
+            # print(lineas)
 opcion1(librox='libros.csv')
 opcion2(lilibro='libros.csv')
-opcion3(addlibro='libros.csv')
+# opcion3(addlibro='libros.csv')
